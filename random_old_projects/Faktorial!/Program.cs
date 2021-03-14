301b6aa83c8bfd4207a45e54e0ee35df0b57519c
Kubafqt <63134204+Kubafqt@users.noreply.github.com>
2021-03-14 23:38:50 +0100

    Add files via upload

---
+++ random_old_projects/Faktorial!/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _Faktorial
+{
+    class Program
+    {
+        //* Toto je méně elegantnější řešení, jelikož cyklus for se blbě přerušuje, ale zase to nehází Infinite loop(stack overflow!) bug. *\\
+        static void Main(string[] args)
+        {
+            Main main = new Main();
+            main.zaklad();
+        }
+    }
+}
